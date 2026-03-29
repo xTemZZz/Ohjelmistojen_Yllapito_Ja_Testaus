ffd19111dd254de9733a1a3c2eed875bfe25f8b6
Teemu Martikainen <teemu.martikainen@student.lab.fi>
2026-03-29 16:42:18 +0300

    Coverage and coveralls added

--- VIII_Deployment/__test__/capitalize.test.js
+++ VIII_Deployment/__test__/capitalize.test.js
@@ -1,26 +1,6 @@
 import { expect } from "chai";
 import capitalize from "../src/capitalize.js"
 
-/**
- * Converts the first character of `string` to upper case and the remaining
- * to lower case.
- *
- * @since 3.0.0
- * @category String
- * @param {string} [string=''] The string to capitalize.
- * @returns {string} Returns the capitalized string.
- * @example
- *
- * capitalize('FRED')
- * // => 'Fred'
- */
-/*const capitalize = (string) => upperFirst(toString(string).toLowerCase())
-
-
-export default capitalize
-
-*/
-
 describe("capitalize.js", () => {
   /**
    * Tests for the camelCase() function
